c1c188f5fe6cd8e7707a42be50f858fc73ab98c7
Ben Evans <ben@bensbit.co.uk>
2015-11-18 02:08:37 +0000

    Fix out of project dir run

--- bin.js
+++ bin.js
@@ -4,4 +4,4 @@ var electron = require('electron-prebuilt')
 
 var proc = require('child_process')
 
-proc.spawn(electron, ['./index.js', process.argv[2]], {stdio: 'inherit'});
+proc.spawn(electron, [__dirname + '/index.js', process.argv[2]], {stdio: 'inherit'});
